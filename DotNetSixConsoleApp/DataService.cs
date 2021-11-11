cb3c715c4d47a9afecd96c84f59a2347e4d9efc5
sahgilbert <sah.gilbert@gmail.com>
2021-11-11 15:08:31 +0000

    Initial commit

---
+++ DotNetSixConsoleApp/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSixConsoleApp
+{
+    internal static class DataService
+    {
+        public static string GetName()
+        {
+            return "Simon";
+        }
+    }
+}
---
+++ DotNetSixConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DotNetSixConsoleApp;
+
+var name = DataService.GetName();
+
+Console.WriteLine($"Hello, {name} and world!");
+
